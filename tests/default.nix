6a7db90db1bdfd46ebf5dbda46dffc6f43fe356f
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-10-31 14:44:50 +0100

    factor out the test function so it can be used to test individual configurations

--- tests/default.nix
+++ tests/default.nix
@@ -1,49 +1,10 @@
-#
-# To run:
-#
-#    nix-build tests/default.nix
-#
-
 let 
   pkgs = import <nixpkgs> {};
   lib = pkgs.lib;
+  makeTest = import ./maketest.nix;
 in
 lib.crossLists 
-  (enableRtnix: kernelVersion: enableRealtime: enableTimerlat: 
-    pkgs.nixosTest({
-      name = "rtnix" + "-${kernelVersion}" + (lib.optionalString enableRealtime "-rt") + (lib.optionalString enableTimerlat "-timerlat");
-  
-      nodes.machine =
-        { config, pkgs, ... }:
-        { imports = [ ../default.nix ];
-  
-          rtnix.enable = enableRtnix;
-          rtnix.kernel.version = kernelVersion;
-          rtnix.kernel.realtime = enableRealtime;
-          rtnix.kernel.timerlat = enableTimerlat;
-        };
-  
-      testScript = ''
-        machine.start()
-        machine.wait_for_unit("default.target")
-      '' 
-      + (lib.optionalString enableRealtime ''
-        result = machine.succeed("uname -v")
-        print(result)
-        if not "PREEMPT RT" or not "PREEMPT_RT" in result:
-            raise Exception("Realtime not enabled")
-      '')
-      + (lib.optionalString enableTimerlat ''
-        result = machine.succeed("zcat /proc/config.gz | grep TIMERLAT")
-        print(result)
-        if not "CONFIG_TIMERLAT_TRACER=y" in result:
-            raise Exception("Missing timerlat tracing")
-      '')
-      + ''
-        print("PASSED")
-      '';
-    })
-  ) 
+  (enableRtnix: kernelVersion: enableRealtime: enableTimerlat: makeTest { inherit enableRtnix kernelVersion enableRealtime enableTimerlat; }) 
  
   # The combinations we test (gets expanded by lib.crossLists above):
   [ 
---
+++ tests/maketest.nix
@@ -0,0 +1,42 @@
+# To test a particular combination run for example:
+#
+# nix-build tests/maketest.nix --argstr kernelVersion 6.0 --arg enableRtnix true --arg enableRealtime true --arg enableTimerlat true
+
+{ enableRtnix, kernelVersion, enableRealtime, enableTimerlat }:
+let 
+  pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
+in
+  pkgs.nixosTest({
+    name = "rtnix" + "-${kernelVersion}" + (lib.optionalString enableRealtime "-rt") + (lib.optionalString enableTimerlat "-timerlat");
+
+    nodes.machine =
+      { config, pkgs, ... }:
+      { imports = [ ../default.nix ];
+
+        rtnix.enable = enableRtnix;
+        rtnix.kernel.version = kernelVersion;
+        rtnix.kernel.realtime = enableRealtime;
+        rtnix.kernel.timerlat = enableTimerlat;
+      };
+
+    testScript = ''
+      machine.start()
+      machine.wait_for_unit("default.target")
+    '' 
+    + (lib.optionalString enableRealtime ''
+      result = machine.succeed("uname -v")
+      print(result)
+      if not "PREEMPT RT" or not "PREEMPT_RT" in result:
+          raise Exception("Realtime not enabled")
+    '')
+    + (lib.optionalString enableTimerlat ''
+      result = machine.succeed("zcat /proc/config.gz | grep TIMERLAT")
+      print(result)
+      if not "CONFIG_TIMERLAT_TRACER=y" in result:
+          raise Exception("Missing timerlat tracing")
+    '')
+    + ''
+      print("PASSED")
+    '';
+  })
